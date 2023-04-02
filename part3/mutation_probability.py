b1eae1b0c6e4aee9c979060f4ff879655c9b5ad0
eohiri <eohiri@unm.edu>
2023-04-02 15:24:43 -0600

    Add files via upload

--- part3/mutation_probability.py
+++ part3/mutation_probability.py
@@ -1,131 +1,134 @@
+# Wrtten in collaboration with Christopher Leap
+
 from neutral_network import table, BasicNeutralNetwork
 import numpy as np
+import pandas as pd
 
-
-filename = 'seq.txt'
-
-f = open(filename, "r")
-seq = ''.join(f.readlines()).replace('\n', '') + 'A'
-f.close()
-
-print(len(seq))
-
-
+# Change number from 1, 2, or 3, for respective n-point strict mutation; leave as 1 for non-strict n-point mutation
 base_muts = BasicNeutralNetwork(1)
 base_muts._build()
 
-#for codon, muts in base_muts.mutations.items():
+#for key, value in base_muts.mutations.items():
     #print(codon+":", muts)
-
 
 idx2aa = list(set(table.values()))
 idx2aa.sort()
-print(idx2aa, len(idx2aa))
+#print(idx2aa, len(idx2aa))
 
 aa2idx = {aa: i for i, aa in enumerate(idx2aa)}
-print(aa2idx)
+#print(aa2idx)
 
 count_table = np.zeros((len(idx2aa), len(idx2aa)))
 
-for condonA, mutsA in base_muts.mutations.items():
+for condonA, muts in base_muts.mutations.items():
     amino = table[condonA]
     row = aa2idx[amino]
-    count_table[row][row] += len(mutsA[0])
+    count_table[row][row] += len(muts[0])
 
-    for codonB in mutsA[1]:
+    for codonB in muts[1]:
         amino = table[codonB]
         col = aa2idx[amino]
         count_table[row][col] += 1
 
 for i, row in enumerate(count_table):
-    print(f"{i:2d} ", end="")
+    print(f"{idx2aa[i]} ", end="")
     for val in row:
         print(f"{int(val):2d} ", end="")
     print()
 
-trans_table1p = count_table/np.sum(count_table, axis=0)
+trans_tablenp = count_table/np.sum(count_table, axis=0)
 
-for i, row in enumerate(trans_table1p):
-    print(f"{i:2d} ", end="")
+for i, row in enumerate(trans_tablenp):
+    print(f"{idx2aa[i]} ", end="")
     for val in row:
         print(f"{val:.2f} ", end="")
     print()
 
+# Uncomment for non-strict mutation
 '''
-trans_table2p = np.dot(trans_table, trans_table)
+trans_table2p = np.dot(trans_tablenp, trans_tablenp)
 
 for i, row in enumerate(trans_table2p):
-    print(f"{i:2d} ", end="")
+    print(f"{idx2aa[i]} ", end="")
     for val in row:
         print(f"{val:.2f} ", end="")
     print()
 
-trans_table3p = np.dot(trans_table2p, trans_table)
+trans_table3p = np.dot(trans_table2p, trans_tablenp)
 
 for i, row in enumerate(trans_table3p):
-    print(f"{i:2d} ", end="")
+    print(f"{idx2aa[i]} ", end="")
     for val in row:
         print(f"{val:.2f} ", end="")
     print()
 '''
 
-#OLD CODE
-'''
-syn_count = 0
-nonsyn_count = 0
-temp = []
-temp2 = []
-temp3 = []
-checked = []
+inpath = "../part2/aamut_fitness_all.csv"
+gene = "S"
+df = pd.read_csv(inpath)
+df["keep"] = df.apply(lambda row: row["gene"] == gene, axis=1)
+df = df[df["keep"]]
 
-for amino, acid in base_muts.mutations.items():
-    if amino in checked:
+#print(df)
+#print(df["clade_founder_aa"].value_counts())
+values = df["clade_founder_aa"].value_counts()
+
+gene_count = np.zeros(len(idx2aa))
+
+for i, amino in enumerate(idx2aa):
+    if amino not in values:
         continue
-    syn_count += len(base_muts.mutations[amino][0])
-    nonsyn_count += len(base_muts.mutations[amino][1])
-    for amino2 in base_muts.mutations.keys():
-        if amino2 == amino:
-            continue
-        if len(acid[0]) != 0:
-            if len(acid[0]) == 3:
-                if acid[0][0] == amino2 or acid[0][1] == amino2 or acid[0][2] == amino2:
-                    syn_count += 3
-                    nonsyn_count += len(acid[1])
-                    checked.append(amino2)
-            elif len(acid[0]) == 2:
-                if acid[0][0] == amino2 or acid[0][1] == amino2:
-                    syn_count += 2
-                    nonsyn_count += len(acid[1])
-                    checked.append(amino2)
-            elif len(acid[0]) == 1:
-                if acid[0] == amino2:
-                    syn_count += 1
-                    nonsyn_count += len(acid[1])
-                    checked.append(amino2)
-        else:
-            continue
-    print(syn_count)
-    temp.append(syn_count)
-    temp2.append(nonsyn_count+syn_count)
-    temp3.append([syn_count, nonsyn_count+syn_count])
-    if len(temp) != 0:
-        if syn_count != temp[-1]:
-            temp.append(syn_count)
     else:
-        temp.append(syn_count)
+        gene_count[i] = values[amino]
+print(gene_count)
 
-    #syn_count = 0
-    #nonsyn_count = 0
+gene_dist = gene_count/np.sum(gene_count)
+print(gene_dist)
 
-for amino in table.values():
-    for amino2 in table.values():
-        if amino == amino2:
-            syn_count += 1
-        else:
-            nonsyn_count += 1
-    print(syn_count, nonsyn_count)
-    syn_count = 0
-    nonsyn_count = 0
+gene_transnp = np.dot(gene_dist, trans_tablenp)
+print(gene_transnp)
+
+# For strict 1- 2- or 3-point mutation, change the argument number on line 17 respectively
+expected_strict_gene_transnp = gene_transnp*np.sum(gene_count)
+print(expected_strict_gene_transnp)
+
+# Uncomment for non-strict mutation
+'''
+nonstrict_gene_trans2p = np.dot(gene_dist, trans_table2p)
+print(nonstrict_gene_trans2p)
+
+nonstrict_gene_trans3p = np.dot(gene_dist, trans_table3p)
+print(nonstrict_gene_trans3p)
 '''
 
+synnonsyn_count_table = np.zeros((len(idx2aa), 2))
 
+for condon, muts in base_muts.mutations.items():
+    amino = table[condon]
+    row = aa2idx[amino]
+    synnonsyn_count_table[row][0] += len(muts[0])
+    synnonsyn_count_table[row][1] += len(muts[1])
+
+for i, row in enumerate(synnonsyn_count_table):
+    print(f"{idx2aa[i]} ", end="")
+    for val in row:
+        print(f"{int(val):2d} ", end="")
+    print()
+
+synonsyn_trans_tablenp = (synnonsyn_count_table.T/np.sum(synnonsyn_count_table.T, axis=0)).T
+
+for i, row in enumerate(synonsyn_trans_tablenp):
+    print(f"{idx2aa[i]} ", end="")
+    for val in row:
+        print(f"{val:.2f} ", end="")
+    print()
+
+synonsyn_gene_trans_table1p = np.dot(gene_dist, synonsyn_trans_tablenp)
+print(synonsyn_gene_trans_table1p)
+
+# For 2- or 3-point mutation, change the argument number on line 17 to 1 or 2, respectively
+synonsyn_gene_trans_tablenp = np.dot(gene_transnp, synonsyn_trans_tablenp)
+print(synonsyn_gene_trans_tablenp)
+
+#temp2aa = np.dot(gene_dist, trans_tablenp)
+#temp2syn = np.dot(gene_dist, trans_table1p2)
